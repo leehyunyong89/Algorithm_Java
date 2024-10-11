2540513ef11edb9a20e11b8abc744c519ba17a4e
leehyunyong89 <leehyunyong89@naver.com>
2024-10-11 16:38:36 +0900

    solution code added

--- LeetCode/Two Sum/src/Main.java
+++ LeetCode/Two Sum/src/Main.java
@@ -4,8 +4,6 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-
         int[] nums = {2, 7, 11, 15};
         int target = 9;
         int[] result = twoSum(nums,target);
