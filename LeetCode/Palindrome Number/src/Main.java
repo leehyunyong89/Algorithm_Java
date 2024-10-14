b140293ec796aae639a7dc2416a9feff96880f04
leehyunyong89 <leehyunyong89@naver.com>
2024-10-14 23:57:57 +0900

    create Palindrome Number Problem Solution

--- LeetCode/Palindrome Number/src/Main.java
+++ LeetCode/Palindrome Number/src/Main.java
@@ -2,16 +2,45 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        Solution solution = new Solution();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Test case 1: Input is a valid palindrome
+        String test1 = "A man, a plan, a canal: Panama";
+        System.out.println("Is palindrome? " + solution.isPalindrome(test1));  // Output: true
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        // Test case 2: Input is not a palindrome
+        String test2 = "race a car";
+        System.out.println("Is palindrome? " + solution.isPalindrome(test2));  // Output: false
+    }
+
+    public static class Solution {
+        public boolean isPalindrome(String s) {
+            int left = 0;
+            int right = s.length() - 1;
+
+            while (left < right) {
+                // Skip non-alphanumeric characters
+                while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                    left++;
+                }
+                while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                    right--;
+                }
+
+                // Compare characters (case insensitive)
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                    return false;
+                }
+
+                // Move pointers towards the center
+                left++;
+                right--;
+            }
+
+            return true;
         }
     }
+
+
+
 }
