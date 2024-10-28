93895ddaa426722ee2d0cc376854639dc4664720
leehyunyong89 <leehyunyong89@naver.com>
2024-10-28 09:31:22 +0900

    Create Folder of Remove Element problem

---
+++ LeetCode/Reomve Element/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Problem of Remove Element");
+    }
+
+
+
+    public static class Solution {
+        public int cntOfSamVals (int val, int[] nums) {
+
+            if (nums.length < 1) return 0;
+
+
+
+            return 0;
+        }
+    }
+
+}
