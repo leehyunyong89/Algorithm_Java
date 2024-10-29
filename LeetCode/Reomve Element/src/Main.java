013affc5c5cfc523cea11ae1fff7cbdedd345de3
leehyunyong89 <leehyunyong89@naver.com>
2024-10-30 08:43:45 +0900

    Edit Solution of Remove Element problem

--- LeetCode/Reomve Element/src/Main.java
+++ LeetCode/Reomve Element/src/Main.java
@@ -21,12 +21,14 @@ public class Main {
     public static class Solution {
         public int removeElement (int[] nums, int val) {
 
-            if (nums.length < 1) return 0;  // 아무값도 없는 nums 배열 들어온 경우 0 리턴.
+            if (nums.length < 1) {
+                return 0; // 아무값도 없는 nums 배열 들어온 경우 0 리턴.
+            }
 
             int j = 0; // 포인터 : 일치하지 않는 값을 입력하기 위한 포인터 인덱스
 
             for (int i = 0; i < nums.length; i++) {
-                if (nums[i] != nums[j]) { // 요소가 val 값과 일치하지 않는다면
+                if (nums[i] != val) { // 요소가 val 값과 일치하지 않는다면
                     nums[j] = nums[i]; // 해당 요소를 j 인덱스 요소값으로 옮기고
                     j++;  // j값 1 증가.
                 }
