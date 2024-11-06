3022a94b9d707066dfb8b600f19c64b43c635348
leehyunyong89 <leehyunyong89@naver.com>
2024-11-06 09:22:29 +0900

    Create Folder and Solution of Length Of Last Word Problem

---
+++ LeetCode/Length Of Last Word/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Problem of Length Of Last Word!");
+
+        String ex01 = "Hello World";
+        String ex02 = "    fly me        to the        moon   ";
+        String ex03 = "luffy is still joyboy";
+
+        Solution solution = new Solution();
+
+        System.out.println(String.format("ex01 result : %s", solution.lengthOfLastWord(ex01)));
+        System.out.println(String.format("ex02 result : %s", solution.lengthOfLastWord(ex02)));
+        System.out.println(String.format("ex03 result : %s", solution.lengthOfLastWord(ex03)));
+
+    }
+
+    public static class Solution{
+        public int lengthOfLastWord(String s) {
+
+            int length = 0;
+            int lastIndexPointer = 0;
+            // 공백 기준 split
+            String[] splitedStr = s.split(" ");
+
+            // 마지막 인덱스 포인터 지정.
+            lastIndexPointer = splitedStr.length-1;
+
+            length = splitedStr[lastIndexPointer].length();
+
+            return length;
+        }
+    }
+}
