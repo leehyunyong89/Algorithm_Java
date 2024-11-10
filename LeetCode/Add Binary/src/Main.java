90738bd07e14cdea38cb3e2eda3570901a007a91
leehyunyong89 <leehyunyong89@naver.com>
2024-11-11 08:58:35 +0900

    Create Temp Solution Class of Add Binary Problem.

--- LeetCode/Add Binary/src/Main.java
+++ LeetCode/Add Binary/src/Main.java
@@ -2,4 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Problem of Add Binary!");
     }
+
+
+    public class Solution{
+        public String addBinary(String a, String b) {
+            String res = null;
+            return res;
+        }
+    }
 }
